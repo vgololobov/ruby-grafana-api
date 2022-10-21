13f239521cd2e68986f47f3a7116b5ae49b8e767
Paul Sopterean <paulsopterean@autouncle.com>
2022-10-21 12:44:32 +0300

    Adjustments to default dashboard template

--- lib/grafana/dashboard_template.rb
+++ lib/grafana/dashboard_template.rb
@@ -11,7 +11,7 @@ module Grafana
     def build_template(params={})
 
       if !params.has_key?('from')
-        params['from'] = 'now-2h'
+        params['from'] = 'now-24h'
       end
       if !params.has_key?('to')
         params['to'] = 'now'
@@ -24,75 +24,41 @@ module Grafana
       params['panels'].each do |panel|
         rows.push(self.build_panel(panel))
       end
-  
+
       tpl = %q[
-      {
-        "dashboard": { 
-          "id": null,
-          "title": "%{title}",
-          "originalTitle": "%{title}",
-          "annotations": {
-            "list": []
-          },
-          "hideControls": false,
-          "timezone": "browser",
-          "editable": true,
-          "rows": [
-            %{rows}
-          ],
-          "time": {
-            "from": "%{from}",
-            "to": "%{to}"
-          },
-          "timepicker": {
-            "collapse": false,
-            "enable": true,
-            "notice": false,
-            "now": true,
-            "refresh_intervals": [
-              "5s",
-              "10s",
-              "30s",
-              "1m",
-              "5m",
-              "15m",
-              "30m",
-              "1h",
-              "2h",
-              "1d"
+        {
+          "dashboard": {
+            "refresh": "30m",
+            "id": null,
+            "title": "%{title}",
+            "annotations": {
+              "list": []
+            },
+            "hideControls": false,
+            "timezone": "",
+            "editable": true,
+            "rows": [
+              %{rows}
             ],
-            "status": "Stable",
-            "time_options": [
-              "5m",
-              "15m",
-              "1h",
-              "6h",
-              "12h",
-              "24h",
-              "2d",
-              "7d",
-              "30d"
-            ],
-            "type": "timepicker"
+            "time": {
+              "from": "%{from}",
+              "to": "%{to}"
+            },
+            "templating": {
+              "list": []
+            },
+            "style": "dark",
+            "version": 5,
+            "links": []
           },
-          "tags": ["api-templated"],
-          "templating": {
-            "list": []
-          },
-          "schemaVersion": 7,
-          "sharedCrosshair": false,
-          "style": "dark",
-          "version": 1,
-          "links": []
-        },
-        "overwrite": false
-      }
+          "overwrite": false
+        }
       ]
 
       return tpl % {
-        title: params['title'], 
-        from: params['from'], 
-        to: params['to'], 
+        title: params['title'],
+        from: params['from'],
+        to: params['to'],
         rows: rows.join(',')
       }
 
@@ -103,70 +69,73 @@ module Grafana
 
       panel = %q[
         {
-          "collapse": false,
           "editable": true,
           "height": "250px",
           "panels": [
             {
-              "aliasColors": {},
-              "bars": false,
-              "datasource": "%{datasource}",
-              "editable": true,
-              "error": false,
-              "fill": 1,
-              "grid": {
-                "leftLogBase": 1,
-                "leftMax": null,
-                "leftMin": null,
-                "rightLogBase": 1,
-                "rightMax": null,
-                "rightMin": null,
-                "threshold1": null,
-                "threshold1Color": "rgba(216, 200, 27, 0.27)",
-                "threshold2": null,
-                "threshold2Color": "rgba(234, 112, 112, 0.22)"
+              "description": "%{description}",
+              "fieldConfig": {
+                "defaults": {
+                  "color": {
+                    "mode": "palette-classic"
+                  },
+                  "custom": {
+                    "axisLabel": "Count",
+                    "axisPlacement": "auto",
+                    "barAlignment": 0,
+                    "drawStyle": "line",
+                    "fillOpacity": 0,
+                    "gradientMode": "none",
+                    "hideFrom": {
+                      "legend": false,
+                      "tooltip": false,
+                      "viz": false
+                    },
+                    "lineInterpolation": "linear",
+                    "lineWidth": 1,
+                    "pointSize": 5,
+                    "scaleDistribution": {
+                      "type": "linear"
+                    },
+                    "showPoints": "auto",
+                    "spanNulls": false,
+                    "stacking": {
+                      "group": "A",
+                      "mode": "none"
+                    },
+                    "thresholdsStyle": {
+                      "mode": "off"
+                    }
+                  },
+                  "mappings": [],
+                  "thresholds": {}
+                }
               },
-              "legend": {
-                "avg": false,
-                "current": false,
-                "max": false,
-                "min": false,
-                "show": true,
-                "total": false,
-                "values": false
+              "gridPos": {
+                "h": %{position_h},
+                "w": %{position_w},
+                "x": %{position_x},
+                "y": %{position_y}
               },
-              "lines": true,
-              "linewidth": 2,
-              "links": [],
-              "nullPointMode": "connected",
-              "percentage": false,
-              "pointradius": 5,
-              "points": false,
-              "renderer": "flot",
-              "seriesOverrides": [],
-              "span": 12,
-              "stack": false,
-              "steppedLine": false,
+              "id": null,
+              "options": {
+                "legend": {
+                  "calcs": [],
+                  "displayMode": "list",
+                  "placement": "bottom"
+                },
+                "tooltip": {
+                  "mode": "single",
+                  "sort": "none"
+                }
+              },
               "targets": [
                 %{targets}
               ],
-              "timeFrom": null,
-              "timeShift": null,
-              "title": "%{graph_title}",
-              "tooltip": {
-                "shared": true,
-                "value_type": "cumulative"
-              },
-              "type": "graph",
-              "x-axis": true,
-              "y-axis": true,
-              "y_formats": [
-                "short",
-                "short"
-              ]
+              "title": "%{title}",
+              "type": "timeseries"
             }
-          ],
-          "title": "Row"
+          ]
         }
       ]
 
@@ -175,45 +144,31 @@ module Grafana
         targets.push(self.build_target(t))
       end
 
-  
       return panel % {
-        datasource: params['datasource'],
-        graph_title: params['graph_title'],
+        description: params['description'],
+        title: params['title'],
+        position_h: params['gridPos']['h'],
+        position_w: params['gridPos']['w'],
+        position_x: params['gridPos']['x'],
+        position_y: params['gridPos']['y'],
         targets: targets.join(',')
       }
-
-
     end
 
-
     def build_target(params={})
-
       target = %q[
         {
-          "alias": "%{legend_alias}",
-          "dimensions": {
-            "%{dimension_name}": "%{dimension_value}"
-          },
-          "metricName": "%{metric_name}",
-          "namespace": "%{namespace}",
-          "period": 60,
-          "query": "",
-          "refId": "A",
-          "region": "%{region}",
-          "statistics": [
-            "Maximum"
-          ],
-          "timeField": "@timestamp"
+          "exemplar": true,
+          "expr": "%{expr}",
+          "interval": "",
+          "legendFormat": "",
+          "refId": "%{refId}"
         }
       ]
 
       return target % {
-        metric_name: params['metric_name'],
-        namespace: params['namespace'],
-        dimension_name: params['dimension_name'],
-        dimension_value: params['dimension_value'],
-        region: params['region'],
-        legend_alias: params['legend_alias'],
+        expr: params['expr'],
+        refId: params['refId']
       }
 
     end
@@ -221,4 +176,3 @@ module Grafana
   end
 
 end
-
